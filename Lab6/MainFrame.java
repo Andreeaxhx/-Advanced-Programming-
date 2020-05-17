6728e99a2591c7ac862981d0b69d39aa10f733db
Andreea Pădurariu <62242417+Andreeaxhx@users.noreply.github.com>
2020-05-17 19:57:01 +0300

    ??

--- Lab6/MainFrame.java
+++ Lab6/MainFrame.java
@@ -7,10 +7,6 @@ import javax.swing.*;
 
 public class MainFrame extends JFrame{
 
-	ConfigPanel configPanel;
-	ControlPanel controlPanel;
-	DrawingPanel canvas;
-	
 	public MainFrame()
 	{
 		super("My Drawing Application");
@@ -19,21 +15,11 @@ public class MainFrame extends JFrame{
 	
 	private void init()
 	{
-		//Main Frame
 		setTitle("Main Frame");
 		setSize(600, 400);
 		setLocation(0, 0);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-		//am initializat obiectele
-		canvas = new DrawingPanel(this);
-		configPanel = new ConfigPanel (this);
-		controlPanel = new ControlPanel (this);
-		//si le-am adaugat in main frame
-		add(canvas, BorderLayout.CENTER);
-		add(configPanel,BorderLayout.NORTH);
-		add(controlPanel,BorderLayout.SOUTH);
 		
 		pack();
 	}
